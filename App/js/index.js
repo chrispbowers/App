812e22b060ed6d3acb92cb62747949b6d27b2b21
IzzyR19 <43641689+IzzyR19@users.noreply.github.com>
2019-03-21 13:39:44 +0000

    Shoved in everything from 11.1

--- App/js/index.js
+++ App/js/index.js
@@ -1,15 +1,32 @@
+//global variables
+var filetext  = "";
+var fileBinding;
+var fileEntry;
+
+//parameters catch information
+//two types of listeners, web and device
+//$(subject).on("command", execute function);
+
+console.log("setting up events");
+
+//setup event listeners
 $(document).on("pagecreate","#pageone", onPageCreated);
 
-//some example data
 books = [
   { name: 'Da Vinci Code', 	author: 'Dan Brown',  describe: 'It is fun and confusing'	},
   { name: 'Shutter Island',  author: 'Dennis Lehane', describe: 'Good plot twist'	},
   { name: 'City of Bones',	author: 'Cassandra Clare', describe: 'Cool plot but wow is the series long'}
 ];
 
+//setup listener for device API load
+document.addEventListener("deviceready", onDeviceReady, false);
 
+// once jQuery page 'pageone' has been created 
 function onPageCreated() {
-	var ractive = new Ractive({
+
+     console.log("page created");
+	
+    var ractive = new Ractive({
 		  // The `el` option can be a node, an ID, or a CSS selector.
 		  el: '#content',
 	
@@ -24,9 +41,91 @@ function onPageCreated() {
     submitText();
   })
 		});
-}
     
-    function Randomise(){
+	//setup buttons
+	$('#writeFile').on("click", writeFile);
+    $('deleteFile').on("Click", deleteFile);
+	
+}
+
+function onDeviceReady() {
+	console.log("device ready");
+	
+    
+    //following allows you to gain access to the supported platform specific locations that are shared by all applications (useful for stioring images, music etc. )
+    window.resolveLocalFileSystemURL(cordova.file.dataDirectory, gotFS, fail);
+    //syncedDataDirectory if you wanna sync to iCloud
+}
+
+//get access to file and CREATE if does not exists
+function gotFS(fileSystem) {
+    
+ 	fileSystem.getFile("test.txt", {create: true, exclusive: false}, gotFileEntry, fail);
+}
+
+//get file entry
+function gotFileEntry(fileEntryURL) {
+	console.log("got file entry");
+	this.fileEntry = fileEntryURL
+	fileEntry.file(gotFile, fail);
+}
+
+//get file itself
+function gotFile(file){
+    console.log("got file");
+	readAsText(file);
+}
+
+//READ text from file - assumes that the file contains 
+function readAsText(file) {
+    console.log("readAsText");
+	
+	var reader = new FileReader();
+	
+	//assigns a callback function to be run once the file has been completely read
+	reader.onloadend = function(evt) {
+	
+		//store the new string in 'filetext'
+		filetext = evt.target.result;
+	
+        $('#textarea').val(filetext);
+        
+    };
+	
+	//begin reading the file
+   	reader.readAsText(file);
+}
+
+
+//UDPATE file contents - called when submit button is pressed
+function writeFile()
+{
+    console.log("writeFile: "  + fileEntry.fullPath);
+    
+    filetext = $('#textarea').val();
+    
+	fileEntry.createWriter(
+		function (writer) { 
+			writer.write(filetext);
+		}, 
+		fail
+	);
+
+}
+
+function deleteFile()
+{
+    console.log("deleteFile: " + fileEntry.fullpath);
+    
+    alert("This button works dw");
+    
+}
+
+function fail(error) {
+	alert("Cannot use file: " + error.message);
+}
+
+function Randomise(){
     var onShake = function(){
     alert("Ow!! Shaking hurts!");
 }
@@ -41,6 +140,7 @@ function Barcode(){
     alert("This button works so far! That's some progress!!");
 }
 
-//I lov it when my comments delete themselves
-//Get Randomise working cus it's not
-//Attempt the whole adding in barcode plugin    
+//Get randomise function working
+//attach to Backendless instead of local
+//Get delete function working
+
